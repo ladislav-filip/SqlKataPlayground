21a2bc5002cfa38de2ab5b30ad9c71b68157d4f9
LF <ladislav.filip@gmail.com>
2021-09-26 09:17:32 +0200

    Inicializace

---
+++ src/SqlKataFirebird/Extensions/HostExtensions.cs
@@ -0,0 +1,45 @@
+﻿#region Info
+
+// FileName:    HostExtensions.cs
+// Author:      Ladislav Filip
+// Created:     26.09.2021
+
+#endregion
+
+using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace SqlKataFirebird.Extensions
+{
+    public static class HostExtensions
+    {
+        public static IHost RunConsole(this IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+
+            var conf = scope.ServiceProvider.GetRequiredService<IConfiguration>();
+
+
+            Console.WriteLine("Console run...");
+            Console.WriteLine("Press Enter...");
+            var input = Console.ReadLine();
+
+            if (input == "stop")
+            {
+                Console.WriteLine("Stopped");
+                Environment.ExitCode = 5000;
+                return host;
+            }
+
+
+            Console.WriteLine("Continue...");
+
+            // zatím web nebudu zkoušet...
+            // host.Run();
+
+            return host;
+        }
+    }
+}
---
+++ src/SqlKataFirebird/Startup.cs
@@ -0,0 +1,36 @@
+﻿#region Info
+
+// FileName:    Startup.cs
+// Author:      Ladislav Filip
+// Created:     26.09.2021
+
+#endregion
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace SqlKataFirebird
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            // app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
